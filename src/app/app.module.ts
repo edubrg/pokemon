4eb6240a1937d30d3265d91567c82d816340d507
edubrg <eduardobrg06@gmail.com>
2022-09-09 12:04:32 -0300

    Ajuste nomenclatura menu-principal para header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IndexModule } from './index/index.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		MenuPrincipalComponent,
+		HeaderComponent,
 		FooterComponent,
 	],
 	imports: [
