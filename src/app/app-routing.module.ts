c3205d1ced55e4d4226e6c86bb756e45fafc5940
edubrg <eduardobrg06@gmail.com>
2022-09-14 18:27:20 -0300

    create page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: IndexComponent
+	},
+	{ 
+		path: '**', 
+		pathMatch: 'full', 
+		component: NotFoundComponent 
 	},
 ];
 
